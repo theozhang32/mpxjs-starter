ebb61b8276d6f5b42e2e7d5c7fe7b5d576885745
张天昊 <tianhaozhang@yntengyun.com>
2023-04-21 14:20:12 +0800

    init

---
+++ vue.config.js
@@ -0,0 +1,24 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  pluginOptions: {
+    mpx: {
+      srcMode: 'wx',
+      plugin: {
+        hackResolveBuildDependencies: ({ files, resolveDependencies }) => {
+          const path = require('path')
+          const packageJSONPath = path.resolve('package.json')
+          if (files.has(packageJSONPath)) files.delete(packageJSONPath)
+          if (resolveDependencies.files.has(packageJSONPath)) {
+            resolveDependencies.files.delete(packageJSONPath)
+          }
+        }
+      },
+      loader: {}
+    }
+  },
+  /**
+   * 如果希望node_modules下的文件时对应的缓存可以失效，
+   * 可以将configureWebpack.snap.managedPaths修改为 []
+   */
+  configureWebpack(config) {}
+})
