c2548ae413e2995b858b37735b34f3ccd730473b
TheoZhang <theozhang.tech@outlook.com>
2024-08-27 18:08:05 +0800

    feat: init

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,13 +1,14 @@
 /*
  * @Author: 张天昊
  * @Date: 2024-08-27 15:30:09
- * @LastEditTime: 2024-08-27 15:58:06
+ * @LastEditTime: 2024-08-27 17:18:36
  * @LastEditors: 张天昊
  * @Description:
  * @FilePath: /mpx-template/src/pages/index.ts
  */
-import { createPage, onLoad } from '@mpxjs/core'
+import { createPage, onLoad, ref } from '@mpxjs/core'
 import { storeToRefs } from '@mpxjs/pinia'
+import Toast from '@vant/weapp/toast/toast'
 import { useSetupStore } from '@/stores/setup'
 import { request } from '@/utils/request'
 
@@ -15,13 +16,37 @@ createPage({
   setup() {
     const setupStore = useSetupStore()
 
+    const columns = ['杭州', '宁波', '温州', '嘉兴', '湖州']
+
+    const show = ref(false)
+
     onLoad((options) => {
       console.log(options)
     })
 
+    const indexValue = ref(0)
+    function onChange(event: WechatMiniprogram.CustomEvent) {
+      const { value } = event.detail
+      Toast(`当前值：${value}`)
+    }
+
+    function showPopup() {
+      show.value = true
+    }
+
+    function onClose() {
+      show.value = false
+    }
+
     return {
       ...storeToRefs(setupStore),
-      onIncrementClick: setupStore.increment
+      show,
+      indexValue,
+      columns,
+      onIncrementClick: setupStore.increment,
+      onChange,
+      showPopup,
+      onClose
     }
   }
 })
